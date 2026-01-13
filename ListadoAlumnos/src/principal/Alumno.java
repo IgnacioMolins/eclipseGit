e4aa86ab53b0f5840720125182122778e0416bd2
Ignacio Molins <ignacio.molins@educa.madrid.org>
2026-01-13 08:49:01 +0100

    Primer commit / Añadida clase Alumno con atributos

---
+++ ListadoAlumnos/src/principal/Alumno.java
@@ -0,0 +1,13 @@
+package principal;
+
+public class Alumno {
+	private String nombre;
+	private String apellidos;
+	private String dni;
+
+	public Alumno(String nombre, String apellidos, String dni) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.dni = dni;
+	}
+}
--- ListadoAlumnos/src/principal/Main.java
+++ ListadoAlumnos/src/principal/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Listado de alumnos ciudad escolar");// TODO Auto-generated method stub
-
+		System.out.println("Alumno1: ");
 	}
 
 }
